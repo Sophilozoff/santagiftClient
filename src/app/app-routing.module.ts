a61d799a7d29c39be12763b29ca5cc55832fbb13
Sophilozoff <sophilozoff@hotmail.fr>
2021-12-14 10:16:19 +0100

    path /allgifts works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {GiftsComponent} from "./gifts/gifts.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'allgifts', component: GiftsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GiftsComponent } from './gifts/gifts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GiftsComponent
   ],
   imports: [
     BrowserModule,
