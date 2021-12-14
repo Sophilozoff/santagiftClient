d65d4bb0c032c57e30cc7b0120dbc94269a1f2d6
Sophilozoff <sophilozoff@hotmail.fr>
2021-12-14 13:38:52 +0100

    modif app.module et refactor gift-card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GiftCardComponent } from './gift-card/gift-card.component';
+import { GiftCardComponent } from './components/gift-card/gift-card.component';
 import {HttpClientModule} from "@angular/common/http";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
