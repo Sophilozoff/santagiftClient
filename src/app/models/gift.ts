97051dcfaaa152106afef9a91f9fbb58bccf5c5d
Sophilozoff <sophilozoff@hotmail.fr>
2021-12-16 14:32:43 +0100

    modif model gift

--- src/app/models/gift.ts
+++ src/app/models/gift.ts
@@ -1,13 +1,20 @@
+import {Timestamp} from "rxjs";
+
 export class Gift {
 
   name: string;
   url: string;
   child: string;
+  santa: string;
+  isBooked: boolean;
+  createdAt!: Timestamp<any>;
 
-
-  constructor(name: string, url: string, child: string) {
+  constructor(name: string, url: string, child: string, santa: string, isBooked: boolean, createdAt: Timestamp<any>) {
     this.name = name;
     this.url = url;
     this.child = child;
+    this.santa = santa;
+    this.isBooked = isBooked;
+    this.createdAt = createdAt;
   }
 }
